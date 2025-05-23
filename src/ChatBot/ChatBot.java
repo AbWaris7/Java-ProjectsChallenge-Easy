8315dc6e81684fc38ec0993ee69d4648144671c3
abwaris7 <hellokhan.ch@gmail.com>
2025-05-23 07:53:34 +0200

    Meet your chat bot

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -3,5 +3,11 @@ package ChatBot;
 public class ChatBot {
     public static void main(String[] args) {
 
+        String name = "Aid";
+        int birthYear = 2023;
+
+        System.out.println("Hello! My name is "+name);
+        System.out.println("I was created in "+birthYear);
+
     }
 }
