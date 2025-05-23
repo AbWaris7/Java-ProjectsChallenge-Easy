f008e7241bd230018fd2560ac0b253be984ee1b2
abwaris7 <hellokhan.ch@gmail.com>
2025-05-23 08:03:11 +0200

    Introduce yourself

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,13 +1,20 @@
 package ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
 
-        String name = "Aid";
-        int birthYear = 2023;
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello! My name is "+name);
-        System.out.println("I was created in "+birthYear);
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+
+        // reading a name
+        String yourName = scanner.next();
+
+        System.out.println("What a great name you have, "+yourName+"!");
 
     }
 }
