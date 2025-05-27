6521dc2ef712ae8f7da25eb585656b7bfc8228cf
abwaris7 <hellokhan.ch@gmail.com>
2025-05-27 23:21:19 +0200

    Print the prices

--- src/AccountManagement/AccountManagement.java
+++ src/AccountManagement/AccountManagement.java
@@ -3,5 +3,12 @@ package AccountManagement;
 public class AccountManagement {
     public static void main(String[] args) {
 
+        System.out.println("Prices:");
+        System.out.println("Bubblegum: $2");
+        System.out.println("Toffee: $0.2");
+        System.out.println("Ice cream: $5");
+        System.out.println("Milk chocolate: $4");
+        System.out.println("Doughnut: $2.5");
+        System.out.println("Pancake: $3.2");
     }
 }
