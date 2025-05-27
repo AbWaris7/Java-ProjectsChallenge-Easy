c85ca51435683880d9aacc12bac640264fc35f6d
abwaris7 <hellokhan.ch@gmail.com>
2025-05-27 23:27:13 +0200

    Calculate net income

--- src/AccountManagement/AccountManagement.java
+++ src/AccountManagement/AccountManagement.java
@@ -3,12 +3,21 @@ package AccountManagement;
 public class AccountManagement {
     public static void main(String[] args) {
 
-        System.out.println("Prices:");
-        System.out.println("Bubblegum: $2");
-        System.out.println("Toffee: $0.2");
-        System.out.println("Ice cream: $5");
-        System.out.println("Milk chocolate: $4");
-        System.out.println("Doughnut: $2.5");
-        System.out.println("Pancake: $3.2");
+        int bubblegum = 202;
+        int toffee = 118;
+        int iceCream = 2250;
+        int milkChocolate = 1680;
+        int doughnut = 1075;
+        int pancake = 80;
+        int income = bubblegum + toffee + iceCream + milkChocolate + doughnut + pancake;
+
+        System.out.println("Earned amount:");
+        System.out.println("Bubblegum: $"+bubblegum);
+        System.out.println("Toffee: $"+toffee);
+        System.out.println("Ice cream: $"+iceCream);
+        System.out.println("Milk chocolate: $"+milkChocolate);
+        System.out.println("Doughnut: $"+doughnut);
+        System.out.println("Pancake: $"+pancake);
+        System.out.println("Income: $"+income);
     }
 }
