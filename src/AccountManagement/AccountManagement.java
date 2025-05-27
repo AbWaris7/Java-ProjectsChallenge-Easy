667fb20fda2c87c98b804cf2f9dbedee3b1011e8
abwaris7 <hellokhan.ch@gmail.com>
2025-05-27 23:57:52 +0200

    Calculate net income

--- src/AccountManagement/AccountManagement.java
+++ src/AccountManagement/AccountManagement.java
@@ -1,7 +1,12 @@
 package AccountManagement;
 
+import java.util.Scanner;
+
 public class AccountManagement {
     public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
 
         int bubblegum = 202;
         int toffee = 118;
@@ -10,7 +15,6 @@ public class AccountManagement {
         int doughnut = 1075;
         int pancake = 80;
         int income = bubblegum + toffee + iceCream + milkChocolate + doughnut + pancake;
-
         System.out.println("Earned amount:");
         System.out.println("Bubblegum: $"+bubblegum);
         System.out.println("Toffee: $"+toffee);
@@ -18,6 +22,15 @@ public class AccountManagement {
         System.out.println("Milk chocolate: $"+milkChocolate);
         System.out.println("Doughnut: $"+doughnut);
         System.out.println("Pancake: $"+pancake);
+
+        System.out.println();
         System.out.println("Income: $"+income);
+        System.out.println("Staff expenses:");
+        int staffExpenses =  scanner.nextInt();
+        System.out.println("Other expenses:");
+        int otherExpenses =   scanner.nextInt();
+        int netIncome =  income - (staffExpenses + otherExpenses);
+        System.out.println("Net income: $"+netIncome);
+
     }
 }
